49a11a5bb0f91e48e635bef96bda8ee7d9a3fb4b
Sam Perlow <maka.laster@gmail.com>
2017-05-25 13:28:15 -0400

    Updated ability holder with string formats

--- AdventureFriends/app/src/main/java/com/makalaster/adventurefriends/player/pages/recyclers/AbilityRecyclerAdapter.java
+++ AdventureFriends/app/src/main/java/com/makalaster/adventurefriends/player/pages/recyclers/AbilityRecyclerAdapter.java
@@ -1,5 +1,6 @@
 package com.makalaster.adventurefriends.player.pages.recyclers;
 
+import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import com.makalaster.adventurefriends.model.character.components.Ability;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Makalaster on 5/22/17.
@@ -33,12 +35,17 @@ public class AbilityRecyclerAdapter extends RecyclerView.Adapter<AbilityHolder> 
     public void onBindViewHolder(AbilityHolder holder, int position) {
         Ability ability = mAbilities.get(position);
 
+        Resources res = holder.mCheck.getContext().getResources();
+
         holder.mName.setText(ability.getName());
-        holder.mLevel.setText(String.valueOf(ability.getLevel()));
-        holder.mDamage.setText(String.valueOf(ability.getDamage()));
-        holder.mRange.setText(String.valueOf(ability.getRange()));
-        holder.mCheck.setText(ability.getCheck());
-        holder.mEffects.setText(ability.getEffects());
+        holder.mLevel.setText(String.format(Locale.ENGLISH, " %s %d",
+                res.getString(R.string.level_label), ability.getLevel()));
+        holder.mDamage.setText(String.format(Locale.ENGLISH, " %d ", ability.getDamage()));
+        holder.mRange.setText(String.format(Locale.ENGLISH, " %d ", ability.getRange()));
+        holder.mCheck.setText(String.format(Locale.ENGLISH, " %s", ability.getCheck()));
+
+        String effects = ability.getEffects();
+        holder.mEffects.setText(String.format(Locale.ENGLISH, " %s", effects == null ? "None" : effects));
         holder.mDescription.setText(ability.getDescription());
         holder.mQuote.setText(ability.getQuote());
     }
