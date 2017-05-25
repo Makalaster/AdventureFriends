7e4cdd400aed524eb888900ace80c038df99425e
Sam Perlow <maka.laster@gmail.com>
2017-05-25 13:09:13 -0400

    Updated player stats page with string formats

--- AdventureFriends/app/src/main/java/com/makalaster/adventurefriends/player/pages/StatsPageFragment.java
+++ AdventureFriends/app/src/main/java/com/makalaster/adventurefriends/player/pages/StatsPageFragment.java
@@ -12,6 +12,8 @@ import com.makalaster.adventurefriends.R;
 import com.makalaster.adventurefriends.model.character.PlayerCharacter;
 import com.makalaster.adventurefriends.player.PlayerCharacterHolder;
 
+import java.util.Locale;
+
 /**
  * A simple {@link Fragment} subclass.
 
@@ -59,18 +61,57 @@ public class StatsPageFragment extends Fragment {
 
         PlayerCharacter currentCharacter = mPlayerCharacterHolder.getPlayerCharacter();
         if (currentCharacter != null) {
-            ((TextView) view.findViewById(R.id.character_name)).setText(currentCharacter.getName());
-            ((TextView) view.findViewById(R.id.character_size)).setText(currentCharacter.getSize().getName());
-            ((TextView) view.findViewById(R.id.character_job)).setText(currentCharacter.getJob().getName());
-            ((TextView) view.findViewById(R.id.current_pg)).setText(String.valueOf(currentCharacter.getCurrentPG()));
-            ((TextView) view.findViewById(R.id.max_pg)).setText(String.valueOf(currentCharacter.getMaxPG()));
-            ((TextView) view.findViewById(R.id.current_xp)).setText(String.valueOf(currentCharacter.getCurrentXP()));
-            ((TextView) view.findViewById(R.id.xp_to_next)).setText(String.valueOf(currentCharacter.getXPtoNext()));
-            ((TextView) view.findViewById(R.id.body_value)).setText(String.valueOf(currentCharacter.getBody()));
-            ((TextView) view.findViewById(R.id.mind_value)).setText(String.valueOf(currentCharacter.getMind()));
-            ((TextView) view.findViewById(R.id.essence_value)).setText(String.valueOf(currentCharacter.getEssence()));
-            ((TextView) view.findViewById(R.id.speed_value)).setText(String.valueOf(currentCharacter.getSpeed()));
-            ((TextView) view.findViewById(R.id.money_value)).setText(String.valueOf(currentCharacter.getMoney()));
+            String name = currentCharacter.getName();
+            ((TextView) view.findViewById(R.id.character_name))
+                    .setText(String.format(Locale.ENGLISH, " %s", name));
+
+            String size = currentCharacter.getSize().getName();
+            ((TextView) view.findViewById(R.id.character_size))
+                    .setText(String.format(Locale.ENGLISH, " %s", size));
+
+            String job = currentCharacter.getJob().getName();
+            ((TextView) view.findViewById(R.id.character_job))
+                    .setText(String.format(Locale.ENGLISH, " %s", job));
+
+            int currentPg = currentCharacter.getCurrentPG();
+            ((TextView) view.findViewById(R.id.current_pg))
+                    .setText(String.format(Locale.ENGLISH, " %d", currentPg));
+
+            int maxPg = currentCharacter.getMaxPG();
+            ((TextView) view.findViewById(R.id.max_pg))
+                    .setText(String.format(Locale.ENGLISH, "%d ", maxPg));
+
+            int level = currentCharacter.getLevel();
+            ((TextView) view.findViewById(R.id.current_level))
+                    .setText(String.format(Locale.ENGLISH, " %d,", level));
+
+            int currentXp = currentCharacter.getCurrentXP();
+            ((TextView) view.findViewById(R.id.current_xp))
+                    .setText(String.format(Locale.ENGLISH, " %d", currentXp));
+
+            int xPToNext = currentCharacter.getXPtoNext();
+            ((TextView) view.findViewById(R.id.xp_to_next))
+                    .setText(String.format(Locale.ENGLISH, "%d ", xPToNext));
+
+            int body = currentCharacter.getBody();
+            ((TextView) view.findViewById(R.id.body_value))
+                    .setText(String.format(Locale.ENGLISH, " %d", body));
+
+            int mind = currentCharacter.getMind();
+            ((TextView) view.findViewById(R.id.mind_value))
+                    .setText(String.format(Locale.ENGLISH, " %d", mind));
+
+            int essence = currentCharacter.getEssence();
+            ((TextView) view.findViewById(R.id.essence_value))
+                    .setText(String.format(Locale.ENGLISH, " %d", essence));
+
+            int speed = currentCharacter.getSpeed();
+            ((TextView) view.findViewById(R.id.speed_value))
+                    .setText(String.format(Locale.ENGLISH, " %d", speed));
+
+            int money = currentCharacter.getMoney();
+            ((TextView) view.findViewById(R.id.money_value))
+                    .setText(String.format(Locale.ENGLISH, " %d", money));
         }
     }
 }
